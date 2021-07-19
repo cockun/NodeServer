957c8c937deae52cbff7c0ab53678829f15a3d8a
Vu Minh Khoa <vuminhkhoa97@gmail.com>
2021-07-19 12:43:52 +0700

    up

--- src/utils/apiCaller.ts
+++ src/utils/apiCaller.ts
@@ -1,34 +1,13 @@
 import axios from "axios";
+import { MomoReq } from "../request/MomoReq";
 
 const API_URL = "http://35.247.162.235/api";
 
-export async function callApi(endpoint, method = "GET", body) {
-  if(method ==="GET"){
-    try{
-     return await axios.get( `${API_URL}/${endpoint}`,{params:  body})
-    }catch(e){
-      return null;
-    }
-  }
-  
+export async function callApi(endpoint:string, method = "GET", body:MomoReq) {
+   
   if(method ==="POST"){
     try{
      return await axios.post( `${API_URL}/${endpoint}`,  {data:body})
-    }catch(e){
-      return null;
-    }
-  }
-
-  if(method ==="UPDATE"){
-    try{
-     return await axios.put( `${API_URL}/${endpoint}`,   {data:body})
-    }catch(e){
-      return null;
-    }
-  }
-  if(method ==="DELETE"){
-    try{
-     return await axios.delete( `${API_URL}/${endpoint}`,  {params:  body})
     }catch(e){
       return null;
     }
